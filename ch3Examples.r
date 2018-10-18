6b1bed1f3f0b47f8361902abbc1f3d9c58d8e52f
Andira Putri <35618440+diramputri@users.noreply.github.com>
2018-10-18 12:31:44 -0400

    Update ch3Examples.r

--- ch3Examples.r
+++ ch3Examples.r
@@ -4,3 +4,10 @@ plot(arima.sim(list(order=c(1,0,0), ar=.9), n=100), ylab="x",
 main=(expression(AR(1)~~~phi==+.9)))
 plot(arima.sim(list(order=c(1,0,0), ar=-.9), n=100), ylab="x",
 main=(expression(AR(1)~~~phi==-.9))
+
+#MA(1) Process
+par(mfrow = c(2,1))
+plot(arima.sim(list(order=c(0,0,1), ma=.5), n=100), ylab="x",
+main=(expression(MA(1)~~~theta==+.5)))
+plot(arima.sim(list(order=c(0,0,1), ma=-.5), n=100), ylab="x",
+main=(expression(MA(1)~~~theta==-.5)))
