fc4dedf98d693e7a4bc3ca53cfd041fcd2e9ce55
Andira Putri <35618440+diramputri@users.noreply.github.com>
2018-10-18 12:32:40 -0400

    Update ch3Examples.r

--- ch3Examples.r
+++ ch3Examples.r
@@ -11,3 +11,9 @@ plot(arima.sim(list(order=c(0,0,1), ma=.5), n=100), ylab="x",
 main=(expression(MA(1)~~~theta==+.5)))
 plot(arima.sim(list(order=c(0,0,1), ma=-.5), n=100), ylab="x",
 main=(expression(MA(1)~~~theta==-.5)))
+
+#Calculate polynomial roots
+z = c(1,-1.5,.75) # coefficients of the polynomial
+(a = polyroot(z)[1]) # print one root: 1+0.57735i = 1 + i/sqrt(3)
+arg = Arg(a)/(2*pi) # arg in cycles/pt
+1/arg # = 12, the pseudo period
